87225e8bbd4c028435981b6a6da807f866c935a3
LindseyBurker <10838835@my.uvu.edu>
2019-09-04 21:39:28 -0600

    added trail, not sure what it did

--- Test/Assets/wowieletsProgram.cs
+++ Test/Assets/wowieletsProgram.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class wowieletsProgram : MonoBehaviour
 {
+    int num = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("hello");
+        //if num = 1{
+        //   int cool = 2  ;
+        //}
+        int babbaba = 12345;
+        //WaitForEndOfFrame.Equals(num);
+
+        enabled.ToString();
+         //idk what any of this code does but it exsists
+       
     }
 
     // Update is called once per frame
