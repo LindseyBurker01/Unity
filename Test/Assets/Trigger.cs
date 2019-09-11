c5ab49688be3c02709566671f0560d5efa3720c6
LindseyBurker <10838835@my.uvu.edu>
2019-09-11 14:06:40 -0600

    forgottocloserideroops

--- Test/Assets/Trigger.cs
+++ Test/Assets/Trigger.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Trigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        triggerEvent.Invoke();
     }
 }
