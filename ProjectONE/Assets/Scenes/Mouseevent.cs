3191fe05844a2cd721d93f10e5b679bdd071be46
Lindseyburker <10838835@my.uvu.edu>
2019-09-12 15:52:21 -0600

    Learning Events with particles

---
+++ ProjectONE/Assets/Scenes/Mouseevent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Mouseevent : MonoBehaviour
+{
+    public UnityEvent mouseEvent;
+
+    private void OnMouseDown()
+    {
+        mouseEvent.Invoke();
+    }
+
+}
