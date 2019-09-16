0a125ab0c5fced68cfbf7a9a4a37c48a6c04c574
LindseyBurker <10838835@my.uvu.edu>
2019-09-15 19:35:49 -0600

    More Particles and Events

---
+++ Test/Assets/TriggerClickEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerClickEvent : MonoBehaviour
+{
+
+    public UnityEvent mouseClick;
+    public void OnMouseDown(Collider other)
+    {
+        mouseClick.Invoke();
+    }
+}
