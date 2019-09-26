78d7fa84994510ce61255eda8de3a8db0ff56d28
LindseyBurker <10838835@my.uvu.edu>
2019-09-26 14:38:55 -0600

    Character movement

---
+++ ProjectONE/Assets/Script/TriggerEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEvent.print("Good Job!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
