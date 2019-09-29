ccb7d2ef1dc9a67fb4d2465856db22a326df6dd6
LindseyBurker <10838835@my.uvu.edu>
2019-09-28 22:17:18 -0600

    Player Movement and Camera Movement

---
+++ Project01/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    private Vector3 position;
+    private CharacterController controller;
+    public float moveSpeed = 10f, gravity = 3f, jumpSpeed = 50f;
+
+    private int jumpCount, jumpCountMax = 1;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        position.y -= gravity;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && jumpCount < jumpCountMax)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+        
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+        }
+
+        controller.Move(position * Time.deltaTime);
+        //controller.minMoveDistance = 0;
+    }
+    
+}
