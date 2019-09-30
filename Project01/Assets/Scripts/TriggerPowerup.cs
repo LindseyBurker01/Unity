a5ba6cd656a219c2276cf9293263b51078a256f1
LindseyBurker <10838835@my.uvu.edu>
2019-09-30 17:57:46 -0600

    Platform and Powerup

---
+++ Project01/Assets/Scripts/TriggerPowerup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements.StyleEnums;
+
+public class TriggerPowerup : MonoBehaviour
+{
+    public float speed = 5f;
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = increasespeed(5f);
+        throw new NotImplementedException();
+    }
+
+    private float increasespeed(float multiplier)
+    {
+        return speed = multiplier;
+    }
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Project01/Assets/Scripts/Triggerenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using EventTrigger = UnityEngine.Analytics.EventTrigger;
+
+public class Triggerenter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
