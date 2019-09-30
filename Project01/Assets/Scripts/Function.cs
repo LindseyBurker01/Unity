f3d95eab90dbd5fffb03d3419d8188ee30673c99
LindseyBurker <10838835@my.uvu.edu>
2019-09-29 22:46:56 -0600

    Functions added

---
+++ Project01/Assets/Scripts/Function.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Function : MonoBehaviour
+{
+    public string playername = "Tester";
+
+    public GameObject player;
+    public Color playerColor = Color.blue;
+    void Start()
+    {
+        configgameobject();
+    }
+
+    private GameObject configgameobject()
+    {
+        var newplayer = player;
+        newplayer.layer = 0;
+        newplayer.tag = "Player";
+        newplayer.name = playername;
+        newplayer.GetComponent<Renderer>().material.color = playerColor;
+        return player;
+    }
+
+}
