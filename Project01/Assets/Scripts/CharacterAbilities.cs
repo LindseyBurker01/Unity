369adfd2155ffac916ebdab27322caf887f00033
LindseyBurker <10838835@my.uvu.edu>
2019-10-04 18:56:37 -0600

    Ammo and Bullets added. Bullets follow player

---
+++ Project01/Assets/Scripts/CharacterAbilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterAbilities : MonoBehaviour
+{
+    public UnityEvent trigger;
+    
+    //public void OnTriggerEnter(Collider other)
+    //{
+      //  trigger.Invoke();
+    //}
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            trigger.Invoke();
+        }
+
+    }
+}
