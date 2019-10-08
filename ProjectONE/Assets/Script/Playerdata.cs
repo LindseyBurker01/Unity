386792983d8a651b9f2937b198d962b498c8d778
Lindseyburker <10838835@my.uvu.edu>
2019-10-08 15:50:52 -0600

    Players and weapons added

---
+++ ProjectONE/Assets/Script/Playerdata.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Playerdata : GameArtData
+{
+    public WeaponData weapon;
+    //So this is configuring characters, and all characters need a sprite, color, and gameobject
+
+    public void InstancePlayer()
+    {
+        var newPlayer = Instantiate(prefab);
+        var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+        newSprite.sprite = sprite;
+        newSprite.color = color;
+    }
+}
