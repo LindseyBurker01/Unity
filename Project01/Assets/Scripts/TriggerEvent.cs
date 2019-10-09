01c0bde8758121d4e63ad2bf9b800121bf175e51
LindseyBurker <10838835@my.uvu.edu>
2019-10-08 22:00:50 -0600

    added enemy and cave

---
+++ Project01/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using EventTrigger = UnityEngine.Analytics.EventTrigger;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+    public void OnTriggerEnter(Collider other)
+    {
+        Event.Invoke();
+    }
+}
