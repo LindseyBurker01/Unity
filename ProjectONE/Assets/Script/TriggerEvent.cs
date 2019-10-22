18f477f20befec465c1dc630cc30fc422e296d50
Lindseyburker <10838835@my.uvu.edu>
2019-10-22 15:53:30 -0600

    Gameartdata lists

--- ProjectONE/Assets/Script/TriggerEvent.cs
+++ ProjectONE/Assets/Script/TriggerEvent.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider))]
 public class TriggerEvent : MonoBehaviour
 {
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
 
     public UnityEvent Event;
     private void OnTriggerEnter(Collider other)
