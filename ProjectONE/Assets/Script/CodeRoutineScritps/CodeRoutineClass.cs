7e0498a238d7ef5872ce9d131d3bc77beed81013
Lindseyburker <10838835@my.uvu.edu>
2019-11-14 15:41:55 -0700

    CodeRoutine Delay Repeat Stop

---
+++ ProjectONE/Assets/Script/CodeRoutineScritps/CodeRoutineClass.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class CodeRoutineClass : MonoBehaviour
+{
+    public bool canRun = true;
+    public IntData index;
+    private ParticleSystem pop;
+    public float sec = 1f;
+    private WaitForSeconds wfsObj;
+
+    public void Start()
+    {
+        pop = GetComponent<ParticleSystem>();
+    }
+
+    public void Run()
+    {
+        wfsObj = new WaitForSeconds(sec);
+        StartCoroutine(OnRun());
+    }
+    IEnumerator OnRun()
+    {
+        while (index.value > 0)
+        {
+            yield return wfsObj;
+            pop.Emit(5);
+            index.value--;
+        }
+    }
+    
+}
